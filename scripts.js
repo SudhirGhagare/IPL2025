efb07bd46b2659b00753bc5ad2666e2bcf8e275a
SudhirGhagare <63112304+SudhirGhagare@users.noreply.github.com>
2025-02-18 07:02:25 +0000

    timer updated - now display countdown

--- scripts.js
+++ scripts.js
@@ -1,26 +1,39 @@
-const countdown = document.getElementById("timer");
-const eventDate = new Date("March 22, 2025 18:00:00").getTime();
+const countdown = document.getElementById("timer-container");
+const eventDate = new Date("March 22, 2025 19:30:00").getTime();
+const now = new Date().getTime();
+const diff = eventDate - now;
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+const time = [days, hours, minutes, seconds]
+let container = document.getElementById("container");
+let timeDivs = [];
 
-setInterval(() => {
+for (let i = 0; i < 4; i++) {
+    let div = document.createElement("div");
+    div.className = "box";
+   // div.innerText = time[i]
+    container.appendChild(div);
+    timeDivs.push(div);
+}
+console.log(timeDivs)
+function updateCountdown() {
+
     const now = new Date().getTime();
     const diff = eventDate - now;
-    
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+    const timeArray = [days, hours, minutes, seconds];
 
-    countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}, 1000);
+    timeDivs.forEach((div, index) => {
+        div.innerText = `${timeArray[index]} `;
+    });
 
+}
 
-let container = document.getElementById("container");
-
-        // Create and append 4 divs
-        for (let i = 1; i <= 4; i++) {
-            let div = document.createElement("div");
-            div.className = "box";
-            container.appendChild(div);
-        }
-
-setInterval(createFirework, 500);
+updateCountdown()
+setInterval(updateCountdown, 1000);
+        

5256ad75d01efffe94a21c67c2baafe550e54774
SudhirGhagare <63112304+SudhirGhagare@users.noreply.github.com>
2025-03-06 17:36:51 +0000

    play-off page created

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,29 +1,49 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { TeamsContainer } from "./components/TeamsContainer";
 import "./App.css";
-import matches from './assets/knock-out.json'
+import matches from "./assets/knock-out.json";
 
 function App() {
 
-  console.log(matches, "matches");
   return (
-    <div className="grid h-full grid-cols-3  gap-4 m-[42px]">
-      <div className="grid grid-rows-2 gap-14">
-        <div className="border-2 border-solid rounded-b-[12px]  border-[#15185f] h-42 w-75 shadow-xl">
-          <strong className="flex justify-center p-1.4 bg-orange-500 text-white text-xl ">
-            {matches[0]["Match Number"]}
-          </strong>
-        </div>
+    <div className="grid h-full  grid-cols-3  gap-4 m-[42px]">
 
-        <div className="border-2 border-solid rounded-b-[12px]  border-[#15185f] h-42 w-75 shadow-xl">
-          <strong className="flex justify-center p-1.4 bg-orange-500 text-white text-xl ">
-            {matches[1]["Match Number"]}
-          </strong>
-        </div>
+      <div className="grid grid-rows-2 gap-2 w-full h-[450px]">
+
+        {/* Qualifier 1 container start */}
+        <TeamsContainer 
+        matchType={matches[0]["Match Number"]} 
+        homeTeam={matches[0]["Home Team"]}
+        awayTeam={matches[0]["Away Team"]}
+        date={matches[0]["Date"]} time={matches[0]['Time']}
+        venue={matches[0]["Venue"]} />
+        {/* Qualifier 1 container end */}
+
+        <TeamsContainer 
+        matchType={matches[1]["Match Number"]} 
+        homeTeam={matches[1]["Home Team"]}
+        awayTeam={matches[1]["Away Team"]}
+        date={matches[1]["Date"]} time={matches[1]['Time']}
+        venue={matches[1]["Venue"]} />
+        
       </div>
-      <div>02</div>
-      <div>03</div>
+      <div className="grid grid-cols-1 mt-24">
+      <TeamsContainer 
+        matchType={matches[2]["Match Number"]} 
+        homeTeam={matches[2]["Home Team"]}
+        awayTeam={matches[2]["Away Team"]}
+        date={matches[2]["Date"]} time={matches[1]['Time']}
+        venue={matches[2]["Venue"]} />
+      </div>
+      <div className="grid grid-cols-1 mt-24">
+
+      <TeamsContainer 
+        matchType={matches[3]["Match Number"]} 
+        homeTeam={matches[3]["Home Team"]}
+        awayTeam={matches[3]["Away Team"]}
+        date={matches[3]["Date"]} time={matches[3]['Time']}
+        venue={matches[3]["Venue"]} />
+
+      </div>
     </div>
   );
 }
